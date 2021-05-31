6bbe8ff90eb19f04ec78aed1601eecc467ca65d4
Jason Antwi-Appah <hey@jasonaa.me>
2021-05-30 21:02:31 -0500

    remove pings

--- src/messages.ts
+++ src/messages.ts
@@ -4,15 +4,15 @@ import getGenericResponse from './genericMessages'
 const filter = new ProfanityFilter()
 
 export function parseUserMessage(msg: string): string {
-  // return msg.replace(/<\@.+>/g, '').trim()
-  return msg.trim()
+  return msg.replace(/<\@.+>/g, '').trim()
+//   return msg.trim()
 }
 
 export function parseChatResponse(msg: string): string {
   const fallback = getGenericResponse('fallbackResponse')
   try {
-    // return filter.clean(msg.replace(/<\@.+>/g, '').trim()) || fallback
-    return filter.clean(msg) || fallback
+    return filter.clean(msg.replace(/<\@.+>/g, '').trim()) || fallback
+//     return filter.clean(msg) || fallback
   } catch (err) {
     console.error(err)
     return fallback
